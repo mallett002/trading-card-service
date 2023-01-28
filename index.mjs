ca53cccfa18aa7206ea252c6aea3e42d604a550f
William Mallett <mallett002@gmail.com>
2023-01-28 08:37:23 -0600

    cleanup

--- index.mjs
+++ index.mjs
@@ -3,12 +3,8 @@ import getCards from './src/handlers/get-cards.mjs';
 import getCard from './src/handlers/get-card.mjs';
 import createCard from './src/handlers/create-card.mjs';
 
-const app = express()
-const port = 3000
-
-// POST /trading-card endpoint
-// GET /trading-card endpoint
-// GET /trading-cards endpoint
+const app = express();
+const port = 3000;
 
 app.post('/card', createCard);
 app.get('/card', getCard);
