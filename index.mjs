f30e6d46478b0b2ea27e659642238b0043981e34
William Mallett <mallett002@gmail.com>
2023-02-07 06:01:50 -0600

    cleanup

--- index.mjs
+++ index.mjs
@@ -3,16 +3,14 @@ import getCards from './src/handlers/get-cards.mjs';
 import getCard from './src/handlers/get-card.mjs';
 import createCard from './src/handlers/create-card.mjs';
 
-// Todo: connect to db: https://node-postgres.com/
-
 const app = express();
-app.use(express.json());
 const port = 3000;
+
+app.use(express.json());
 
 app.post('/card', createCard);
 app.get('/card/:id', getCard);
 app.get('/cards', getCards);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
