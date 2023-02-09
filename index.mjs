7b4d66a208739e85ab2d30efdef001de71d0a94f
William Mallett <mallett002@gmail.com>
2023-02-09 06:16:30 -0600

    Bump compose version; connect db on startup;

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import getPgClient from './src/db-client.mjs';
 import getCards from './src/handlers/get-cards.mjs';
 import getCard from './src/handlers/get-card.mjs';
 import createCard from './src/handlers/create-card.mjs';
@@ -13,5 +14,7 @@ app.get('/card/:id', getCard);
 app.get('/cards', getCards);
 
 app.listen(port, () => {
+  console.log('Connecting to the database...');
+  getPgClient();
   console.log(`Example app listening on port ${port}`)
 });
--- src/db-client.mjs
+++ src/db-client.mjs
@@ -8,8 +8,8 @@ const getPgClient = () => {
     if (!client) {
         try {
             client = DataApiClient({
-                secretArn: 'arn:aws:secretsmanager:us-east-1:175849613020:secret:AuroraSecret41E6E877-Xl1leH2alD8v-1wRBti',
-                resourceArn: 'arn:aws:rds:us-east-1:175849613020:cluster:tradingcardservicestack-auroracluster23d869c0-atyjp7k6w4ba',
+                secretArn: '',
+                resourceArn: '',
                 database: 'TradingCardDb',
                 region: 'us-east-1',
                 engine: 'pg'
