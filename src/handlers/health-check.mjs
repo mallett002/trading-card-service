2bb3669d158b5b9122ebe992a9d4a765f908524d
William Mallett <mallett002@gmail.com>
2023-04-01 07:42:27 -0500

    return 500 if not healthy

--- src/handlers/health-check.mjs
+++ src/handlers/health-check.mjs
@@ -3,20 +3,18 @@ import getPgClient from '../db-client.mjs';
 const healthCheck = async (req, res) => {
     const client = getPgClient();
 
-    let postgresHealthy = null;
-
     try {
         await client.query('SELECT');
     
-        postgresHealthy = true;
-      } catch (err) {
-        console.log({err});
-        postgresHealthy = false;
-      }
+        return res.status(200).json({
+            postgresHealthy: true
+        });
 
-    res.status(200).json({
-      postgresHealthy
-    });
+      } catch (err) {
+        return res.status(500).json({
+            postgresHealthy: false
+        });
+      }
 };
 
 export default healthCheck;
